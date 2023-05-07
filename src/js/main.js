6518ff506e98c5a7561587333fa2a36828d942bd
Karol <pandamax.karol.lichon@gmail.com>
2023-05-07 20:23:40 +0200

    added page offerts, page contact, scroll-spy

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,113 @@
-const btn = document.querySelector('.plate2')
+let btnNavPlate, nav, btnNav, linksNav, footerYear ,sections
 
-btn.addEventListener('click',()=>{
-    btn.classList.toggle('active')
-})
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+}
 
-const nawigacja = document.querySelector('.nav__list')
-const btn1 = document.querySelector('.nav__btn')
+const prepareDOMElements = () => {
+	btnNavPlate = document.querySelector('.plate2')
+	nav = document.querySelector('.nav__list')
+	btnNav = document.querySelector('.nav__btn')
+	linksNav = document.querySelectorAll('.nav__link')
+	footerYear = document.querySelector('.footer__year')
+    sections = document.querySelectorAll('.section')     
+}
 
-btn1.addEventListener('click', ()=>{
-    nawigacja.classList.toggle('actived')
-    nawigacja.classList.contains('actived')?nawigacja.classList.remove('deactivate'):nawigacja.classList.add('deactivate')
-})
+const prepareDOMEvents = () => {
+	btnNav.addEventListener('click', showNav)
+	linksNav.forEach((el) => {
+		el.addEventListener('click', hiddenNav)
+	})
+    sections.forEach(section => {
+        observer.observe(section)
+    })  
+    handleCurrentYear()
+}
+
+const showNav = () => {
+	btnNavPlate.classList.toggle('active')
+	nav.classList.toggle('actived')
+
+	if (btnNavPlate.classList.contains('active')) {
+		nav.classList.remove('deactivate')
+		document.body.style.overflow = 'hidden'
+	} else {
+		nav.classList.add('deactivate')
+		document.body.style.overflow = 'auto'
+	}
+}
+
+const hiddenNav = () => {
+	document.body.style.overflow = 'auto'
+	btnNavPlate.classList.toggle('active')
+	nav.classList.remove('actived')
+	nav.classList.add('deactivate')
+}
+
+const options = {
+    root: null,
+    threshold: 0.9
+};
+
+const activateNavLink = (entries)=>{
+    entries.forEach(entry=>{
+        if(entry.isIntersecting){
+            const id = entry.target.getAttribute('id');
+            addActiveClass(id);
+        }
+    })
+}
+
+const addActiveClass = (id)=> {
+    linksNav.forEach(link => {
+      if (link.getAttribute('href') === `index.html#${id}`) {
+        link.classList.add('nav__link--active');
+      } else {
+        link.classList.remove('nav__link--active');
+      }
+    });
+}
+
+const observer = new IntersectionObserver(activateNavLink, options)
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.textContent = year
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+
+////////////////////////////////////////////////to delete
+// const sections = document.querySelectorAll('.section')
+
+// const options = {
+//     root: null,
+//     threshold: 0.9
+// };
+
+// const addActiveClass = (entries)=>{
+//     entries.forEach(entry=>{
+//         if(entry.isIntersecting){
+//             const id = entry.target.getAttribute('id');
+//             activateNavLink(id);
+//         }
+//     })
+// }
+
+// const activateNavLink = (id)=> {
+//     linksNav.forEach(link => {
+//       if (link.getAttribute('href') === `index.html#${id}`) {
+//         link.classList.add('nav__link--active');
+//       } else {
+//         link.classList.remove('nav__link--active');
+//       }
+//     });
+// }
+
+// const observer = new IntersectionObserver(addActiveClass, options)
+
+// sections.forEach(section => {
+//     observer.observe(section)
+// })
